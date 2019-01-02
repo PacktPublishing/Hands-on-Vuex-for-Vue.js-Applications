1ef60c7f983e2a0c5cfd51bb6ad70ed03999c5ba
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-02 15:49:56 -0600

    init section 1

---
+++ section-1-vuex-api/.prettierrc.js
@@ -0,0 +1,6 @@
+// We're using the default config for all sections
+// The `npm run lint` command runs prettier via eslint, so
+// this config isn't really necessary unless you're running prettier
+// manually (like through a plugin in your editor).
+
+module.exports = {};
