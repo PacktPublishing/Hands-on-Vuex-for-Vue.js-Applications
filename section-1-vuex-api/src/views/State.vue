fae20bf6ec9cbc3d33c34ef6782b4533be767d6f
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-03 11:16:32 -0600

    add state lesson

--- section-1-vuex-api/src/views/State.vue
+++ section-1-vuex-api/src/views/State.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
     <h1>Checking the value of <code>count</code></h1>
+    <ul>
+      <li>Count using <code>$store</code>: {{ $store.state.count }}</li>
+      <li>Count using <code>import store</code>: {{ store.state.count }}</li>
+      <li>Count using <code>mapState</code>: {{ count }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+import store from "@/store";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return { store };
+  },
+
+  computed: {
+    ...mapState(["count"])
+  }
+};
+</script>
+
