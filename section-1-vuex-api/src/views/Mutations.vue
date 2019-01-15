63c3173e0e27361bf6d66eb11157738590c9c7ee
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-15 09:32:05 -0700

    add demo

--- section-1-vuex-api/src/views/Mutations.vue
+++ section-1-vuex-api/src/views/Mutations.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     <h1>Changing <code>count</code></h1>
+
+    <p>Count is <strong>{{ $store.state.count }}</strong></p>
+
+    <button class="pure-button"
+            @click="$store.commit('INCREMENT')">Increment</button>
+    <button class="pure-button"
+            @click="$store.commit('ADD', 10)">Add 10</button>
   </div>
 </template>
