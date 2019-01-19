13a65a705157ad4c64f78a90e15b506faa83c5e2
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-19 14:08:03 -0700

    add getters

--- section-1-vuex-api/src/views/Getters.vue
+++ section-1-vuex-api/src/views/Getters.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Hello, World</h1>
+  <div>
+    <h2>Getters</h2>
+
+    <div>Count is {{ $store.state.count }}</div>
+    <div>2x count is {{ $store.getters.doubledCount }}</div>
+    <div>Count + 10 is {{ $store.getters.countPlusN(10) }}</div>
+
+    <button class="pure-button"
+            @click="$store.dispatch('addToCount', 1)">
+      Increment
+    </button>
+  </div>
 </template>
