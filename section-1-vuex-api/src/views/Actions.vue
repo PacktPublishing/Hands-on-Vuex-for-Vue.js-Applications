bdd0d76c3d251a61d1def105223b52ba47e1821e
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-19 12:06:03 -0700

    add pre_actions

--- section-1-vuex-api/src/views/Actions.vue
+++ section-1-vuex-api/src/views/Actions.vue
@@ -1,3 +1,12 @@
 <template>
-  <h2>Changing <code>count</code> with actions</h2>
+  <div>
+    <h2>Changing <code>count</code> with actions</h2>
+
+    <div>Count is {{ $store.state.count }}</div>
+
+    <button class="pure-button"
+            @click="$store.commit('INCREMENT')">
+      Increment
+    </button>
+  </div>
 </template>
