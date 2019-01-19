15fdf8dbcc93779cbb450c6c17827fa5d453f2c3
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-01-19 14:12:04 -0700

    pre getters

--- section-1-vuex-api/src/views/Getters.vue
+++ section-1-vuex-api/src/views/Getters.vue
@@ -3,8 +3,6 @@
     <h2>Getters</h2>
 
     <div>Count is {{ $store.state.count }}</div>
-    <div>2x count is {{ $store.getters.doubledCount }}</div>
-    <div>Count + 10 is {{ $store.getters.countPlusN(10) }}</div>
 
     <button class="pure-button"
             @click="$store.dispatch('addToCount', 1)">
