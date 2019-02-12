de753f2663329c7f31147bd1d4c74f032d115f1b
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-11 21:24:02 -0700

    add item to list

--- section-2-shopping-list/src/components/ShoppingList.vue
+++ section-2-shopping-list/src/components/ShoppingList.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
+    <h3>Add Item</h3>
+    <form class="pure-form pure-form-aligned">
+      <fieldset>
+        <div class="pure-control-group">
+          <label for="name">Item name</label>
+          <input type="text" v-model="newItem.name" id="name" />
+        </div>
+
+        <div class="pure-control-group">
+          <label for="price">Item Price</label>
+          <input type="number" v-model="newItem.price" id="price" />
+        </div>
+
+        <div class="pure-control-group">
+          <label for="quantity">Item Quantity</label>
+          <input type="number" v-model="newItem.quantity" id="quantity" />
+        </div>
+
+        <div class="pure-controls">
+          <button
+            @click="submitForm"
+            type="button"
+            class="pure-button pure-button-primary"
+          >
+            Add Item
+          </button>
+        </div>
+      </fieldset>
+    </form>
+
+    <h3>Items</h3>
     <table class="pure-table">
       <thead>
         <th>Item Name</th>
@@ -8,8 +39,7 @@
       </thead>
 
       <tbody>
-        <tr v-for="item in $store.state.shoppingList"
-            :key="item.id">
+        <tr v-for="item in $store.state.shoppingList" :key="item.id">
           <td>{{ item.name }}</td>
           <td>{{ item.price }}</td>
           <td>{{ item.quantity }}</td>
@@ -18,3 +48,26 @@
     </table>
   </div>
 </template>
+
+<script>
+const INITIAL_ITEM = {
+  name: "",
+  price: null,
+  quantity: 1
+};
+
+export default {
+  data() {
+    return {
+      newItem: INITIAL_ITEM
+    };
+  },
+
+  methods: {
+    submitForm() {
+      this.$store.commit("ADD_ITEM_TO_LIST", this.newItem);
+      this.newItem = INITIAL_ITEM;
+    }
+  }
+};
+</script>
