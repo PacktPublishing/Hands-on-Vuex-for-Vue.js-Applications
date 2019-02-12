01183cc3d05665fc5919ab44391d0fa0d1576c0b
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-11 21:08:29 -0700

    add view for shopping list

---
+++ section-2-shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <table class="pure-table">
+      <thead>
+        <th>Item Name</th>
+        <th>Price</th>
+        <th>Quantity</th>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in $store.state.shoppingList"
+            :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.quantity }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- section-2-shopping-list/src/store.js
+++ section-2-shopping-list/src/store.js
@@ -7,7 +7,7 @@ let uuid = 0;
 
 export default new Vuex.Store({
   state: {
-    shoppingList: [] // Array of items with { id, name, price, quantity}
+    shoppingList: [{ id: 2, name: "apples", price: 0.5, quantity: 5 }] // Array of items with { id, name, price, quantity}
   },
 
   mutations: {
