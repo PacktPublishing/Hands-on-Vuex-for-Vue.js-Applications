65854e2088d647c346b520a8c4048f21af9769cd
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-11 21:08:29 -0700

    add view for shopping list

--- section-2-shopping-list/src/App.vue
+++ section-2-shopping-list/src/App.vue
@@ -4,15 +4,21 @@
       <div class="header">
         <h1>Shopping List</h1>
       </div>
-      <div class="content"></div>
+      <div class="content">
+        <shopping-list></shopping-list>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ShoppingList from "./components/ShoppingList.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    ShoppingList
+  }
 };
 </script>
 
