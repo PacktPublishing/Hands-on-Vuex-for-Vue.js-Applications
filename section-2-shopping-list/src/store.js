ae748f16b8a82df794e1f096fb561836ebb0f39e
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-11 21:02:30 -0700

    add mutations and state

--- section-2-shopping-list/src/store.js
+++ section-2-shopping-list/src/store.js
@@ -3,8 +3,30 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+let uuid = 0;
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    shoppingList: [] // Array of items with { id, name, price, quantity}
+  },
+
+  mutations: {
+    ADD_ITEM_TO_LIST(state, { name, price, quantity }) {
+      state.shoppingList.push({
+        id: uuid++,
+        name,
+        quantity: parseInt(quantity),
+        price: parseInt(price)
+      });
+    },
+
+    REMOVE_ITEM_FROM_LIST(state, { id }) {
+      const itemIndex = state.shoppingList.find(item => item.id === id);
+      state.shoppingList.slice(itemIndex, 1);
+    },
+
+    EDIT_ITEM_NAME(state, { item, newName }) {
+      item.name = newName;
+    }
+  }
 });
