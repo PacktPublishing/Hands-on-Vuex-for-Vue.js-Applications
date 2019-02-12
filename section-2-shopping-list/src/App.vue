0d2c56b663dcdb90e4e0e9bc4bbbf5ab7b0e069e
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-11 18:06:57 -0700

    section 2 init

---
+++ section-2-shopping-list/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <div class="header">
+        <h1>Shopping List</h1>
+      </div>
+      <div class="content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+
+  .wrapper {
+    margin: 10px;
+  }
+}
+</style>
