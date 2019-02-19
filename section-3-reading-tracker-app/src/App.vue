34daaa34a5db519add976d19b55e760c29226fab
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-19 12:24:55 -0700

    add boilerplate

---
+++ section-3-reading-tracker-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <div class="pure-menu pure-menu-horizontal">
+        <ul class="pure-menu-list"></ul>
+      </div>
+    </div>
+
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  padding: 0.5em 1em;
+}
+</style>
