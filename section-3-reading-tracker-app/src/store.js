d8d1d8c30aabbb6041b131979fa15f390c51ceec
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 10:15:07 -0700

    Add books display

--- section-3-reading-tracker-app/src/store.js
+++ section-3-reading-tracker-app/src/store.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import books from "./books.json";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  strict: process.env.NODE_ENV === "production",
+
+  state: {
+    books: books, // array of { title, description, author, lists (string array) }
+
+    lists: [], // array of strings
+
+    users: []
+  },
+
   mutations: {},
+
   actions: {}
 });
