edcbeb316d2e4b01af772fbfed91d00cd0743653
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 11:04:17 -0700

    add placeholder lists

--- section-3-reading-tracker-app/src/store.js
+++ section-3-reading-tracker-app/src/store.js
@@ -1,18 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import books from "./books.json";
+import books from "./data/books.json";
+import lists from "./data/lists.json";
 
 Vue.use(Vuex);
 
-let nextId = 0;
+let nextId = 200;
 export default new Vuex.Store({
   strict: process.env.NODE_ENV === "production",
 
   state: {
-    books: books, // array of { id, title, description, author, lists (string array) }
+    books: books, // array of { id, title, description, author, lists (array of ids) }
 
-    lists: [], // array of { id, name, description }
+    lists: lists, // array of { id, name, description }
 
     users: []
   },
