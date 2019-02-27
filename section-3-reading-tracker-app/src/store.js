8ef2f87797c33eedaa07fc7b4e3ec81e0ad07308
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 20:41:45 -0700

    populate initial data

--- section-3-reading-tracker-app/src/store.js
+++ section-3-reading-tracker-app/src/store.js
@@ -1,12 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import books from "./data/books.json";
+import lists from "./data/lists.json";
+
 Vue.use(Vuex);
 
+let nextId = 200;
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
 
-  state: {},
+  state: {
+    books, // Array of { id, title, author, pageCount, publishedDate, lists }
+
+    lists // Array of { id, name, description }
+  },
 
   mutations: {}
 });
