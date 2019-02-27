d6172352dd4e2a56643a70af0f97478651908d81
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 17:49:48 -0700

    WIP add list tagging

--- section-3-reading-tracker-app/src/components/BooksList.vue
+++ section-3-reading-tracker-app/src/components/BooksList.vue
@@ -15,6 +15,9 @@
       <div class="column is-1">
         <strong>Published</strong>
       </div>
+      <div class="column is-one-third">
+        <strong>Lists</strong>
+      </div>
     </div>
 
     <div class="columns" v-for="book in books" :key="book.id">
@@ -29,6 +32,18 @@
       </div>
       <div class="column is-1">
         {{ new Date(book.publishedDate).getFullYear() }}
+      </div>
+      <div class="column">
+        <b-field>
+          <b-taginput
+            placeholder="Add to list"
+            autocomplete
+            :data="$store.state.lists"
+            field="name"
+            type="is-primary"
+          >
+          </b-taginput>
+        </b-field>
       </div>
     </div>
   </div>
