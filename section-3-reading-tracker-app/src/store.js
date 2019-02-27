fb9d4a411ae54a0dce146daf2cda3345dd671953
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 20:35:40 -0700

    remove some extra things from pre_3.1

--- section-3-reading-tracker-app/src/store.js
+++ section-3-reading-tracker-app/src/store.js
@@ -1,22 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import books from "./data/books.json";
-import lists from "./data/lists.json";
-
 Vue.use(Vuex);
 
-let nextId = 200;
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
 
-  state: {
-    books: books, // array of { id, title, author, pageCount, publishedDate, lists (array of lists) }
-
-    lists: lists, // array of { id, name, description }
-
-    users: []
-  },
+  state: {},
 
   mutations: {}
 });
