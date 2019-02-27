9d9a8087faf6781e4384426006b562ef3a66fc06
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-02-26 19:53:23 -0700

    refactor books list into component

---
+++ section-3-reading-tracker-app/src/components/BooksList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <strong>Title</strong>
+      </div>
+      <div class="column">
+        <strong>Author</strong>
+      </div>
+      <div class="column is-1">
+        <strong>
+          Pages
+        </strong>
+      </div>
+      <div class="column is-1">
+        <strong>Published</strong>
+      </div>
+    </div>
+
+    <div class="columns" v-for="book in books" :key="book.id">
+      <div class="column">
+        {{ book.title }}
+      </div>
+      <div class="column">
+        {{ book.author }}
+      </div>
+      <div class="column is-1">
+        {{ book.pageCount }}
+      </div>
+      <div class="column is-1">
+        {{ new Date(book.publishedDate).getFullYear() }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    books: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
--- section-3-reading-tracker-app/src/views/Books.vue
+++ section-3-reading-tracker-app/src/views/Books.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
     <h1 class="title">Books</h1>
+
+    <books-list :books="currentBooksPage"></books-list>
+    <b-pagination
+      :total="$store.state.books.length"
+      :current.sync="currentPage"
+      :per-page="itemsPerPage"
+    >
+    </b-pagination>
   </div>
 </template>
+
+<script>
+import BooksList from "../components/BooksList.vue";
+
+export default {
+  components: {
+    BooksList
+  },
+
+  data() {
+    return {
+      itemsPerPage: 10,
+      currentPage: 1
+    };
+  },
+
+  computed: {
+    currentBooksPage() {
+      return this.$store.state.books.slice(
+        (this.currentPage - 1) * this.itemsPerPage,
+        this.currentPage * this.itemsPerPage
+      );
+    }
+  }
+};
+</script>
