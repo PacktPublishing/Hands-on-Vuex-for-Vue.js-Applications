33775b80e1fd53f1ce0c0c62234941d869d1a314
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-02 18:10:43 -0700

    add add user functionality

--- section-3-reading-tracker-app/src/views/AddUser.vue
+++ section-3-reading-tracker-app/src/views/AddUser.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1 class="title">Add User</h1>
+  <div>
+    <h1 class="title">Add User</h1>
+    <b-field label="Name">
+      <b-input placeholder="Jane Austen" v-model="name"></b-input>
+    </b-field>
+    <b-field label="Bio">
+      <b-input
+        type="textarea"
+        placeholder="Briefly describe yourself"
+        v-model="bio"
+        expanded
+      ></b-input>
+    </b-field>
+    <button
+      class="button is-primary"
+      @click="addNewUser"
+      :disabled="!name.length || !bio.length"
+    >
+      Add
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      bio: ""
+    };
+  },
+
+  methods: {
+    addNewUser() {
+      this.$store.commit("ADD_USER", { name: this.name, bio: this.bio });
+      this.name = "";
+      this.bio = "";
+    }
+  }
+};
+</script>
