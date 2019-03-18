e8707f190c92f717471cc07058e4269044cad623
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-18 11:53:08 -0600

    add custome store boilerplate

--- section-4-async-actions/src/store.js
+++ section-4-async-actions/src/store.js
@@ -1,17 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-Vue.use(Vuex);
-
 function asyncTimeout(resolveWith, timeout = 500) {
   return new Promise(resolve =>
     setTimeout(() => resolve(resolveWith), timeout)
   );
 }
 
+Vue.use(Vuex);
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
+
   state: {},
+
   mutations: {},
+
   actions: {}
 });
