c5d5ad818a1f19cbd21e71823762e8859d1679c6
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-19 19:18:26 -0600

    cleanup some boilerplate

--- section-4-async-actions/src/App.vue
+++ section-4-async-actions/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "app"
 };
 </script>
 
 <style>
+body {
+  height: 100vh;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#app p {
+  font-size: 2em;
 }
 </style>
