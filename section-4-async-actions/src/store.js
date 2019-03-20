f53b0a8f9c63324f16287a6325f6d9f0d9387d86
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-19 19:22:17 -0600

    add async timeout helper

--- section-4-async-actions/src/store.js
+++ section-4-async-actions/src/store.js
@@ -3,6 +3,12 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+function asyncTimeout(resolveWith, timeout = 500) {
+  return new Promise(resolve =>
+    setTimeout(() => resolve(resolveWith), timeout)
+  );
+}
+
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   state: {},
