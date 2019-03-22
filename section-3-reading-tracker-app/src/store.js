6196c7d7fac4d7eff74da4e54c9aa164d9c0a78b
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-22 14:21:33 -0600

    get API setup

--- section-3-reading-tracker-app/src/store.js
+++ section-3-reading-tracker-app/src/store.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import books from "./data/books.json";
-import lists from "./data/lists.json";
-import users from "./data/users.json";
-
 Vue.use(Vuex);
 
 const TEMP_USER_ID = -1;
@@ -13,11 +9,11 @@ export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
 
   state: {
-    books, // Array of { id, title, author, pageCount, publishedDate }
-    users: users, // Array of { id, name, bio, lists (array of { id, name, description, books }) }
+    books: [], // Array of { id, title, author, pageCount, publishedDate }
+    users: [], // Array of { id, name, bio, lists (array of { id, name, description, books }) }
     currentUser: {
       id: TEMP_USER_ID,
-      lists: lists.map(list => Object.assign(list, { books: [] }))
+      lists: []
     }
   },
 
--- section-3-reading-tracker-app/vue.config.js
+++ section-3-reading-tracker-app/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:3001",
+        pathRewrite: { "^/api": "" } // requests to /api/a will be transformed to /a
+      }
+    }
+  }
 };
