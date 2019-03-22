9bae83033f6fca064ee3065611b1d60d1d3301e5
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-22 14:21:33 -0600

    get API setup

--- section-3-reading-tracker-app/backend/server.js
+++ section-3-reading-tracker-app/backend/server.js
@@ -2,20 +2,18 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const books = require("./books.json");
-const users = require("./users.json");
-const lists = require("./lists.json");
 
 const server = jsonServer.create();
 const router = jsonServer.router({
-  users,
-  books,
-  lists
+  users: [], // Array of {id, name, bio, email, password (encrypted), listIds (array) }
+  lists: [], // Array of  { id, name, description, bookIds (array) }
+  books
 });
 const middlewares = jsonServer.defaults();
 
 server.db = router.db;
 
 server.use(middlewares);
-server.use(auth)
+server.use(auth);
 server.use(router);
 server.listen(3001);
