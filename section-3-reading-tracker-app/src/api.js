b6527a4fa129f033db8a707d627c8ffdac5edc9c
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-03-23 20:01:48 -0600

    add update list

--- section-3-reading-tracker-app/src/api.js
+++ section-3-reading-tracker-app/src/api.js
@@ -36,3 +36,7 @@ export function createList(newList) {
 export function getLists() {
   return axios.get(`/api/lists`).then(extractData);
 }
+
+async function updateList(listId, update) {
+  return axios.patch(`/api/lists/${listId}`, update).then(extractData);
+}
