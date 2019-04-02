c00a15a3c1df960310d09e73d5d0231e22765a5f
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-01 18:00:31 -0600

    section 5 boilerplate

---
+++ section-5-modules/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Introducing Modules</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
---
+++ section-5-modules/src/store.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state: {},
+  mutations: {},
+  actions: {}
+});
