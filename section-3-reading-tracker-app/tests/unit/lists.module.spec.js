5dd83c97c9f7b1e43056db38cf4f5ebe707cdba1
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-08 21:37:27 -0600

    make mutation tests not depend on previous unit tests

--- section-3-reading-tracker-app/tests/unit/lists.module.spec.js
+++ section-3-reading-tracker-app/tests/unit/lists.module.spec.js
@@ -1,8 +1,11 @@
 import store from "@/store";
 import mockData from "./mockData";
+import { resetState } from "./helpers";
 import { types as mutations } from "@/store/lists/mutations";
 
 describe("Mutations", () => {
+  beforeEach(resetState);
+
   it("Should set lists", () => {
     store.commit(`lists/${mutations.SET_LISTS}`, [mockData.TEST_LIST]);
 
@@ -11,7 +14,6 @@ describe("Mutations", () => {
   });
 
   it("Should create list", () => {
-    store.commit(`lists/${mutations.SET_LISTS}`, []);
     store.commit(`lists/${mutations.CREATE_LIST}`, mockData.TEST_LIST);
 
     expect(store.state.lists.lists.length).toBe(1);
@@ -19,6 +21,8 @@ describe("Mutations", () => {
   });
 
   it("Should add book to list", () => {
+    store.commit(`lists/${mutations.CREATE_LIST}`, mockData.TEST_LIST);
+
     store.commit(`lists/${mutations.ADD_BOOK_TO_LIST}`, {
       book: mockData.BOOKS[0],
       list: store.state.lists.lists[0]
@@ -29,10 +33,17 @@ describe("Mutations", () => {
   });
 
   it("Should remove book from list", () => {
+    store.commit(`lists/${mutations.CREATE_LIST}`, mockData.TEST_LIST);
+    store.commit(`lists/${mutations.ADD_BOOK_TO_LIST}`, {
+      book: mockData.BOOKS[0],
+      list: store.state.lists.lists[0]
+    });
+
     store.commit(`lists/${mutations.REMOVE_BOOK_FROM_LIST}`, {
       book: mockData.BOOKS[0],
       list: store.state.lists.lists[0]
     });
+
     expect(store.state.lists.lists[0].books.length).toBe(0);
   });
 });
