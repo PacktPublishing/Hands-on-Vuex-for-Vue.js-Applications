e5c0346c7293fe11b16e2cdf9c120c82c83bdbe6
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-08 18:43:14 -0600

    rename fakeData -> testData

--- section-3-reading-tracker-app/tests/unit/lists.module.spec.js
+++ section-3-reading-tracker-app/tests/unit/lists.module.spec.js
@@ -1,37 +1,37 @@
 import store from "@/store";
-import * as fakeData from "./helpers";
+import * as mockData from "./helpers";
 import { types as mutations } from "@/store/lists/mutations";
 
 describe("Mutations", () => {
   it("Should set lists", () => {
-    store.commit(`lists/${mutations.SET_LISTS}`, [fakeData.TEST_LIST]);
+    store.commit(`lists/${mutations.SET_LISTS}`, [mockData.TEST_LIST]);
 
     expect(store.state.lists.lists.length).toBe(1);
-    expect(store.state.lists.lists[0]).toBe(fakeData.TEST_LIST);
+    expect(store.state.lists.lists[0]).toBe(mockData.TEST_LIST);
   });
 
   it("Should create list", () => {
     store.commit(`lists/${mutations.SET_LISTS}`, []);
-    store.commit(`lists/${mutations.CREATE_LIST}`, fakeData.TEST_LIST);
+    store.commit(`lists/${mutations.CREATE_LIST}`, mockData.TEST_LIST);
 
     expect(store.state.lists.lists.length).toBe(1);
-    expect(store.state.lists.lists[0]).toBe(fakeData.TEST_LIST);
+    expect(store.state.lists.lists[0]).toBe(mockData.TEST_LIST);
   });
 
   it("Should add book to list", () => {
     store.commit(`lists/${mutations.ADD_BOOK_TO_LIST}`, {
-      book: fakeData.BOOKS[0],
-      list: fakeData.TEST_LIST
+      book: mockData.BOOKS[0],
+      list: mockData.TEST_LIST
     });
 
     expect(store.state.lists.lists[0].books.length).toBe(1);
-    expect(store.state.lists.lists[0].books[0]).toBe(fakeData.BOOKS[0]);
+    expect(store.state.lists.lists[0].books[0]).toBe(mockData.BOOKS[0]);
   });
 
   it("Should remove book from list", () => {
     store.commit(`lists/${mutations.REMOVE_BOOK_FROM_LIST}`, {
-      book: fakeData.BOOKS[0],
-      list: fakeData.TEST_LIST
+      book: mockData.BOOKS[0],
+      list: mockData.TEST_LIST
     });
     expect(store.state.lists.lists[0].books.length).toBe(0);
   });
--- section-3-reading-tracker-app/tests/unit/user.module.spec.js
+++ section-3-reading-tracker-app/tests/unit/user.module.spec.js
@@ -1,20 +1,20 @@
 import store from "@/store";
 import axios from "axios";
-import * as fakeData from "./helpers";
+import * as mockData from "./helpers";
 import { types as mutations } from "@/store/user/mutations";
 
 describe("Mutations", () => {
   it("Should correctly set token", () => {
-    store.commit(mutations.SET_TOKEN, fakeData.AUTH_RESPONSE.accessToken);
+    store.commit(mutations.SET_TOKEN, mockData.AUTH_RESPONSE.accessToken);
 
-    expect(store.state.user.token).toBe(fakeData.AUTH_RESPONSE.accessToken);
+    expect(store.state.user.token).toBe(mockData.AUTH_RESPONSE.accessToken);
     expect(axios.defaults.headers.common["Authorization"]).toBe(
-      `Bearer ${fakeData.AUTH_RESPONSE.accessToken}`
+      `Bearer ${mockData.AUTH_RESPONSE.accessToken}`
     );
   });
 
   it("Should correctly set current user", () => {
-    store.commit(mutations.SET_CURRENT_USER, fakeData.NEW_USER);
-    expect(store.state.user.current).toBe(fakeData.NEW_USER);
+    store.commit(mutations.SET_CURRENT_USER, mockData.NEW_USER);
+    expect(store.state.user.current).toBe(mockData.NEW_USER);
   });
 });
