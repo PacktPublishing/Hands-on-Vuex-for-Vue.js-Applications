25edb159ee3998caaee101e9f1620e6e4d5e6b45
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-08 18:14:39 -0600

    get users tests working

--- section-3-reading-tracker-app/tests/unit/user.module.spec.js
+++ section-3-reading-tracker-app/tests/unit/user.module.spec.js
@@ -3,6 +3,11 @@ import store from "@/store";
 import mockData from "../mockData";
 import { resetState } from "./helpers";
 import { types as mutations } from "@/store/user/mutations";
+import { types as actions } from "@/store/user/actions";
+import actionImpls from "@/store/user/actions";
+import { types as listsMutations } from "@/store/lists/mutations";
+
+jest.mock("@/api.js");
 
 describe("Mutations", () => {
   beforeEach(resetState);
@@ -19,5 +24,53 @@ describe("Mutations", () => {
   it("Should correctly set current user", () => {
     store.commit(mutations.SET_CURRENT_USER, mockData.NEW_USER);
     expect(store.state.user.current).toEqual(mockData.NEW_USER);
+  });
+});
+
+describe("Actions", () => {
+  beforeEach(resetState);
+
+  it("Should register new user", async () => {
+    await store.dispatch(actions.REGISTER_USER, NEW_USER);
+
+    expect(store.state.user.token).toBeTruthy();
+    expect(store.state.user.current.id).toBeDefined();
+  });
+
+  it("Should login", async () => {
+    await store.dispatch(actions.LOGIN, NEW_USER);
+
+    expect(store.state.user.token).toBeTruthy();
+    expect(store.state.user.current.id).toBeDefined();
+  });
+
+  it("Should fetch user lists", async () => {
+    const commit = jest.fn();
+
+    await actionImpls[actions.GET_USER_LISTS]({
+      commit,
+      dispatch: () => {},
+      state: {
+        current: { id: 0 }
+      }
+    });
+
+    expect(commit).toBeCalledTimes(1);
+    expect(commit).lastCalledWith(`lists/${listsMutations.SET_LISTS}`, [], {
+      root: true
+    });
+
+    await actionImpls[actions.GET_USER_LISTS]({
+      commit,
+      dispatch: () => {},
+      state: {
+        current: { id: 0 }
+      }
+    });
+
+    expect(commit).toBeCalledTimes(2);
+    expect(commit).lastCalledWith(`lists/${listsMutations.SET_LISTS}`, [], {
+      root: true
+    });
   });
 });
