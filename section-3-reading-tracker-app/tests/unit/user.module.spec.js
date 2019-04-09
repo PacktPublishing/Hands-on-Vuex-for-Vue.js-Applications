a8478ae7517cdcfaa0a796686b820569ddf2e109
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-08 21:14:00 -0600

    user tests complete

--- section-3-reading-tracker-app/tests/unit/user.module.spec.js
+++ section-3-reading-tracker-app/tests/unit/user.module.spec.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import store from "@/store";
 import mockData from "../mockData";
 import { resetState } from "./helpers";
+
+import { types as rootActions } from "@/store/actions";
 import { types as mutations } from "@/store/user/mutations";
 import { types as actions } from "@/store/user/actions";
 import actionImpls from "@/store/user/actions";
@@ -45,30 +47,43 @@ describe("Actions", () => {
   it("Should fetch user lists", async () => {
     const commit = jest.fn();
 
-    await actionImpls[actions.GET_USER_LISTS]({
+    const fakeStoreContext = {
       commit,
       dispatch: () => {},
       state: {
         current: { id: 0 }
       }
-    });
+    };
 
+    await actionImpls[actions.GET_USER_LISTS](fakeStoreContext);
     expect(commit).toBeCalledTimes(1);
-    expect(commit).lastCalledWith(`lists/${listsMutations.SET_LISTS}`, [], {
-      root: true
-    });
+    expect(commit).lastCalledWith(
+      `lists/${listsMutations.SET_LISTS}`,
+      expect.anything(),
+      expect.anything()
+    );
 
-    await actionImpls[actions.GET_USER_LISTS]({
-      commit,
-      dispatch: () => {},
-      state: {
-        current: { id: 0 }
-      }
-    });
-
+    await actionImpls[actions.GET_USER_LISTS](fakeStoreContext);
     expect(commit).toBeCalledTimes(2);
-    expect(commit).lastCalledWith(`lists/${listsMutations.SET_LISTS}`, [], {
-      root: true
-    });
+    expect(commit).lastCalledWith(
+      `lists/${listsMutations.SET_LISTS}`,
+      expect.anything(),
+      expect.anything()
+    );
+  });
+
+  it("Should populate list books", async () => {
+    await store.dispatch(rootActions.LOAD_BOOKS);
+    store.commit(`lists/${listsMutations.SET_LISTS}`, [
+      Object.assign(mockData.TEST_LIST, { bookIds: [1, 2] }),
+      Object.assign(mockData.TEST_LIST, { bookIds: [3, 4] })
+    ]);
+
+    store.dispatch(actions.POPULATE_LIST_BOOKS);
+
+    expect(store.state.lists.lists.map(list => list.books)).toEqual([
+      [expect.objectContaining({ id: 1 }), expect.objectContaining({ id: 2 })],
+      [expect.objectContaining({ id: 3 }), expect.objectContaining({ id: 4 })]
+    ]);
   });
 });
