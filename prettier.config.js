09af4d7fdc77638afa5cecef45b1dcb7f26aa3a7
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-17 22:34:14 -0600

    add prettier config at root

---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+// Vue CLI runs prettier via eslint when `npm run lint` is run. This
+// config file should help if you're running prettier manually, like
+// through a IDE plugin
+module.exports = {};
