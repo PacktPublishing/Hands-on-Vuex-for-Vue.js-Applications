948be051c3acb09b112d3570bf7a5c8c51158d89
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-18 11:15:30 -0600

    section 7 boilerplate

---
+++ section-7-plugins/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Check the Console</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
