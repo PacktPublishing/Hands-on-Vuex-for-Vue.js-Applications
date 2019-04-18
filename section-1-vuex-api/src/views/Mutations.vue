a43bc7fa7f4dd9fe2580a697a158f5757aa698bf
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-17 20:42:32 -0600

    1.3 lint

--- section-1-vuex-api/src/views/Mutations.vue
+++ section-1-vuex-api/src/views/Mutations.vue
@@ -3,12 +3,10 @@
     <h1>Changing <code>count</code></h1>
 
     <div>Count is {{ $store.state.count }}</div>
-    <button class="pure-button"
-            @click="$store.commit('INCREMENT')">
+    <button class="pure-button" @click="$store.commit('INCREMENT')">
       Increment
     </button>
-    <button class="pure-button"
-            @click="$store.commit('ADD_N', 10)">
+    <button class="pure-button" @click="$store.commit('ADD_N', 10)">
       Add 10
     </button>
   </div>
