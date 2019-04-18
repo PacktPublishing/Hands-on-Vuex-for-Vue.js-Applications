8ad65bd3cc538ca30587cf8426dc1fa52fea05be
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2019-04-17 20:41:46 -0600

    pre 1.1: lint

--- section-1-vuex-api/src/App.vue
+++ section-1-vuex-api/src/App.vue
@@ -3,34 +3,41 @@
     <div class="header">
       <div class="pure-menu pure-menu-horizontal">
         <ul class="pure-menu-list">
-          <router-link tag="li"
-                       active-class="pure-menu-selected"
-                       class="pure-menu-item"
-                       to="/state">
+          <router-link
+            tag="li"
+            active-class="pure-menu-selected"
+            class="pure-menu-item"
+            to="/state"
+          >
             <a class="pure-menu-link">State</a>
           </router-link>
 
-          <router-link tag="li"
-                       active-class="pure-menu-selected"
-                       class="pure-menu-item"
-                       to="/mutations">
+          <router-link
+            tag="li"
+            active-class="pure-menu-selected"
+            class="pure-menu-item"
+            to="/mutations"
+          >
             <a class="pure-menu-link">Mutations</a>
           </router-link>
 
-          <router-link tag="li"
-                       active-class="pure-menu-selected"
-                       class="pure-menu-item"
-                       to="/getters">
+          <router-link
+            tag="li"
+            active-class="pure-menu-selected"
+            class="pure-menu-item"
+            to="/getters"
+          >
             <a class="pure-menu-link">Getters</a>
           </router-link>
 
-          <router-link tag="li"
-                       active-class="pure-menu-selected"
-                       class="pure-menu-item"
-                       to="/actions">
+          <router-link
+            tag="li"
+            active-class="pure-menu-selected"
+            class="pure-menu-item"
+            to="/actions"
+          >
             <a class="pure-menu-link">Actions</a>
           </router-link>
-
         </ul>
       </div>
     </div>
@@ -38,7 +45,6 @@
     <div class="content">
       <router-view />
     </div>
-
   </div>
 </template>
 
